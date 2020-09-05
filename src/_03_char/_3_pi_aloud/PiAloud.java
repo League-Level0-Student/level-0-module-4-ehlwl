9ca356de168b410fcf4ff2092ba9905a34a435ba
ehlwl <60280351+ehlwl@users.noreply.github.com>
2020-09-05 15:47:50 -0700

    Update PiAloud.java

--- src/_03_char/_3_pi_aloud/PiAloud.java
+++ src/_03_char/_3_pi_aloud/PiAloud.java
@@ -6,9 +6,21 @@ import java.util.Scanner;
 public class PiAloud {
 
 	// 1. Make a main method and make sure your program can run
-
+	public static void main(String[] args) {
+	String p = "3.14159265358979323846";
+	 System.out.print(p.charAt(0));
+	 System.out.print(p.charAt(1));
+	 System.out.print(p.charAt(2));
+	 System.out.println("");
+	 for(int i=0;i<p.length();i++) {
+		 System.out.print(p.charAt(i));
+		 speak(p.charAt(i));
+	 }
+	 
+	 
+	}
 	// 2. Make a String variable to hold 20 digits of Pi. You could use http://www.piday.org/million/ to get this.
-
+	
 	// 3. Print out 3 digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
 	// Check the console shows 3.1
 
